4906adfc3a6c97f648c9d9bbab76860b88896e01
SSelt <sebastian.seltmann@rokomail.de>
2015-09-30 14:55:59 +0200

    updated token + postRequests

--- App/www/js/dbAPI.js
+++ App/www/js/dbAPI.js
@@ -1,6 +1,10 @@
 var dbAPI = (function() {
-	var access_token = 'ca8cc066843b29c86e4fb08054d4ef91';
-	var access_token = 'b1e3f6f4db98e5799ca7433f33c1f5'; //1 - Hans März
+	//var access_token = 'ca8cc066843b29c86e4fb08054d4ef91';
+	//var access_token = 'b1e3f6f4db98e5799ca7433f33c1f5'; //1 - Hans März
+	//dbAPI.setToken('b1e3f6f4db98e5799ca7433f33c1f5'); //1 - Hans März
+	
+	var access_token = 'f3f1c6ef823371ede1fe4db7f13f73';
+	
 	var apiURI = "https://thebankapi.com:8443/api/0.1.0";
 	var getRequest = function(url) {
 		var deferred = $.Deferred();
@@ -15,10 +19,30 @@ var dbAPI = (function() {
 				deferred.resolve(data);
 			},
 			error: function(response) {
-				deferred.reject(response);
+				deferred.reject(response.responseJSON);
 			}
 		});
 		return deferred.promise();
+	}
+	var postRequest = function(url, data) {
+		var deferred = $.Deferred();
+		$.ajax({
+			url: url,
+			type: 'POST',
+			dataType: 'json',
+			data: data,
+			beforeSend: function(xhr) {
+				xhr.setRequestHeader('Authorization', 'Bearer '+access_token);
+			},
+			success: function(data) {
+				deferred.resolve(data);
+			},
+			error: function(response) {
+				deferred.reject(response.responseJSON);
+			}
+		});
+		return deferred.promise();
+		
 	}
 	dbAPI = {
 		contacts: {
@@ -38,6 +62,16 @@ var dbAPI = (function() {
 			put: function(id, contactdata){
 				return; //yolo
 			}
+		},
+		cashAccounts: {
+			makeTransaction:function(accountID, data, callback){
+				getRequest("https://thebankapi.com:8443/api/0.1.0/cashAccounts/"+accountID+"/fundTransfers",data)
+				.done(function(data) 	 {callback(true, data, null);})
+				.fail(function(response) {callback(false, null, response);})
+			}
+		},
+		setToken: function(token) {
+			access_token = token;
 		}
 	};
 	return dbAPI; 
