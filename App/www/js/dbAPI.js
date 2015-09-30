4657844b94617b3f7564c34bdba257d130ec5978
SSelt <sebastian.seltmann@rokomail.de>
2015-09-30 14:32:24 +0200

    dbAPI sendet jetzt korrektes Format an Callback

--- App/www/js/dbAPI.js
+++ App/www/js/dbAPI.js
@@ -1,20 +1,21 @@
 var dbAPI = (function() {
 	var access_token = 'ca8cc066843b29c86e4fb08054d4ef91';
 	var access_token = 'b1e3f6f4db98e5799ca7433f33c1f5'; //1 - Hans März
+	var apiURI = "https://thebankapi.com:8443/api/0.1.0";
 	var getRequest = function(url) {
 		var deferred = $.Deferred();
 		$.ajax({
 			url: url,
 			type: 'GET',
 			dataType: 'json',
+			beforeSend: function(xhr) {
+				xhr.setRequestHeader('Authorization', 'Bearer '+access_token);
+			},
 			success: function(data) {
 				deferred.resolve(data);
 			},
 			error: function(response) {
-				deferred.reject(response.responseJSON);
-			},
-			beforeSend: function(xhr) {
-				xhr.setRequestHeader('Authorization', 'Bearer '+access_token);
+				deferred.reject(response);
 			}
 		});
 		return deferred.promise();
@@ -22,24 +23,14 @@ var dbAPI = (function() {
 	dbAPI = {
 		contacts: {
 			getAll:function(callback) {
-				//var promise = getRequest("https://thebankapi.com:8443/api/0.1.0/contacts");
-				//promise
-				getRequest("https://thebankapi.com:8443/api/0.1.0/contacts")
-				.done(function(data) {
-					callback(data);
-				})
-				.fail(function(response) {
-					callback(response);
-				})
-				//).done(function(data) {
-				//	console.log();
-				//}).fail(function(response) {
-				//	console.log();
-				//});
-					
+				getRequest(apiURI+ "/contacts")
+				.done(function(data) 	 {callback(true, data, null);})
+				.fail(function(response) {callback(false, null, response);})
 			},
-			get: function(id) {
-				return "totally the "+id+"-contact!!1";
+			get: function(id, callback) {
+				getRequest("https://thebankapi.com:8443/api/0.1.0/contacts/"+id)
+				.done(function(data) 	 {callback(true, data, null);})
+				.fail(function(response) {callback(false, null, response);})
 			},
 			post: function(contactdata){
 				return 5; //the id
